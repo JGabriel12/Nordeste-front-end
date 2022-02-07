c64efc986693764c76d2a5865e96f8894c97cc17
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-02-07 17:04:41 -0300

    Bug fixes

--- resources/js/mesas.js
+++ resources/js/mesas.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
   $.get('http://127.0.0.1:8000/ShowMesa', function (mesa) {
-    console.log(mesa)
+    mesa.forEach(function (item) {
+      $('#id_mesa').append(
+        '<option value="' +
+          item.id +
+          '" selected="selected">Mesa ' +
+          item.numero_mesa +
+          '</option>'
+      )
+    })
   })
 })
 
