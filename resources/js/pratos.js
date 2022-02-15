fbf4faba7417e43cd81b5845f4b077790c0ed2e6
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-02-15 14:29:15 -0300

    Price pratos and load screen

--- resources/js/pratos.js
+++ resources/js/pratos.js
@@ -3,7 +3,14 @@ $(document).ready(function () {
   $.get('http://127.0.0.1:8000/ShowPrato', function (prato) {
     prato.forEach(function (item) {
       $('#id_prato').append(
-        '<option value="' + item.id + '"> ' + item.nome_prato + '</option>'
+        '<option value="' +
+          item.id +
+          '"> ' +
+          item.nome_prato +
+          ' |  ' +
+          item.valor_prato +
+          ' R$' +
+          '</option>'
       )
     })
   })
