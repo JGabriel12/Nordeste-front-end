bec0725b6b3a4f55b1318d90eec72039d5ae1a5f
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-03-10 12:24:16 -0300

    updates

--- resources/js/pratos.js
+++ resources/js/pratos.js
@@ -3,17 +3,23 @@ $(document).ready(function () {
   $.get('http://127.0.0.1:8000/ShowPrato', function (prato) {
     prato.forEach(function (item) {
       $('#id_prato').append(
-        '<option value="' + item.id + '">' + item.nome_prato + ' |  ' + item.valor_prato + ' R$' + '</option>'
+        '<option value="' +
+          item.id +
+          '">' +
+          item.nome_prato +
+          ' |  ' +
+          item.valor_prato +
+          ' R$' +
+          '</option>'
       )
     })
+    console.log(valor_prato)
   })
   var string = location.href.split('=').pop()
   $('#id_mesa').val(string)
 
   var string2 = location.href.split('a=').pop()
   $('#id_estadia').val(string2)
-
-  console.log(string2)
 })
 
 $('h1.titulo').hide()
